4ad7636c67b3c1b31d498b5c39f1e8dba62e5b5f
MTolmachev <mikhaeltolmachev@gmail.com>
2023-08-28 18:21:18 +0300

    init

---
+++ Source/SnakeSB/SnakeSBGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SnakeSBGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKESB_API ASnakeSBGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
